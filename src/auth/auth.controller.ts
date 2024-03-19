ccac77bb84c901bc061cf24ec334c0aa9ddf2303
이현종 <jong2@ihyeonjong-ui-MacBookPro.local>
2024-03-20 00:59:26 +0900

    :sparkles: Feat: login api 추가
    Mock 작성해서 테스트 진행 로그인 기능 추가(추후 추가 작업 필요)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { loginDto } from './dto/auth.input';
 
-@Controller()
-export class AuthController {}
+@Controller('api/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() { email, password }: loginDto): Promise<object> {
+    const result = await this.authService.login({ email, password });
+    return result;
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+interface IAuthServiceLogin {
+    email: string;
+    password: string;
+} 
+
+@Injectable()
+export class AuthService {
+    async login({email, password}: IAuthServiceLogin): Promise<object>{
+        if(email == "test" && password == "password123"){
+            return {statusCode: 201, message: "로그인에 성공하였습니다!"};
+        }
+        else {
+            return {statusCode: 401, message: '유효하지 않습니다!'}
+        }
+
+    }
+
+}
