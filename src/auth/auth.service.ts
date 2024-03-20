fdab522be53f641897fd31f3c7b585956adce34d
이현종 <jong2@ihyeonjong-ui-MacBookPro.local>
2024-03-20 22:48:56 +0900

    :wrench: Change: prettier 적용
    tapwidth 2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 interface IAuthServiceLogin {
-    email: string;
-    password: string;
-} 
+  email: string;
+  password: string;
+}
 
 @Injectable()
 export class AuthService {
-    async login({email, password}: IAuthServiceLogin): Promise<object>{
-        if(email == "test" && password == "password123"){
-            return {statusCode: 201, message: "로그인에 성공하였습니다!"};
-        }
-        else {
-            return {statusCode: 401, message: '유효하지 않습니다!'}
-        }
-
+  async login({ email, password }: IAuthServiceLogin): Promise<object> {
+    if (email == 'test' && password == 'password123') {
+      return { statusCode: 201, message: '로그인에 성공하였습니다!' };
+    } else {
+      return { statusCode: 401, message: '유효하지 않습니다!' };
     }
-
+  }
 }
