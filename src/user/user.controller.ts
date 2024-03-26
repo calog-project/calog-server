87d5cbe51590b4a8b1255490c32bb54318fe404e
Lee Gyumin <legm0310@gmail.com>
2024-03-27 04:21:24 +0900

    ✨ Feat: signup API 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { SignupDto } from './dto/user.input';
 
 @Controller('api/user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('signup')
+  async signup(@Body() signupDto: SignupDto): Promise<object> {
+    const result = await this.userService.createUser(signupDto);
+    return result;
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { SignupDto } from './dto/user.input';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  construtor() {}
+
+  async findOne() {}
+  async createUser(dto: SignupDto): Promise<object> {
+    //validate
+    //if(getUserByEmail) throw new HttpException
+    const user = { ...dto };
+    //패스워드 암호화
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(dto.password, salt);
+    user.password = hash;
+
+    return user;
+  }
+}
