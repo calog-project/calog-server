703efd67ba38f071691d09951a4d647003dfc79b
Lee Gyumin <legm0310@gmail.com>
2024-07-31 02:07:06 +0900

    📦️ Chore: kakao oauth, app url 환경변수 설정 추가

--- src/common/config/app.config.ts
+++ src/common/config/app.config.ts
@@ -2,6 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export type AppConfig = {
   nodeEnv: string;
+  url: string;
   port: number;
   apiPrefix: string;
 };
@@ -9,6 +10,7 @@ export type AppConfig = {
 export default registerAs<AppConfig>('app', () => {
   return {
     nodeEnv: process.env.NODE_ENV || 'development',
+    url: process.env.APP_SERVER_URL,
     port: process.env.APP_PORT ? parseInt(process.env.APP_PORT, 10) : 5000,
     apiPrefix: process.env.API_PREFIX || 'api',
   };
--- src/common/config/auth/auth.config.ts
+++ src/common/config/auth/auth.config.ts
@@ -9,6 +9,9 @@ export type AuthConfig = {
   googleClientSecret: string;
   googleCallbackUrl: string;
   googleScope: Array<string>;
+  kakaoClientId: string;
+  kakaoClientSecret: string;
+  kakaoCallbackUrl: string;
 };
 
 export default registerAs<AuthConfig>('auth', () => {
@@ -27,5 +30,9 @@ export default registerAs<AuthConfig>('auth', () => {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     googleCallbackUrl: process.env.GOOGLE_CALLBACK_URL,
     googleScope: [process.env.GOOGLE_SCOPE_1, process.env.GOOGLE_SCOPE_2],
+    //kakao
+    kakaoClientId: process.env.KAKAO_CLIENT_ID,
+    kakaoClientSecret: process.env.KAKAO_CLIENT_SECRET,
+    kakaoCallbackUrl: process.env.KAKAO_CALLBACK_URL,
   };
 });
