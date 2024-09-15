ae9b2ae1c6bc264197be1e39cb00d92a30eb6067
Lee Gyumin <legm0310@gmail.com>
2024-09-15 20:16:36 +0900

    📦️ Chore: 린트 수정 및 패키지 추가, 파일 업로드 관련 설정 추가

--- src/common/config/config.type.ts
+++ src/common/config/config.type.ts
@@ -2,10 +2,12 @@ import { AppConfig } from './app.config';
 import { AuthConfig } from './auth/auth.config';
 import { RedisConfig } from './cache/redis.config';
 import { DatabaseConfig } from './database/database.config';
+import { FileConfig } from './file/file.config';
 
 export type AllConfigType = {
   app: AppConfig;
   db: DatabaseConfig;
   redis: RedisConfig;
   auth: AuthConfig;
+  file: FileConfig;
 };
--- src/common/config/file/file.config.ts
+++ src/common/config/file/file.config.ts
@@ -1,8 +1,18 @@
 import { registerAs } from '@nestjs/config';
 
 // @TODO validation
-export type FileConfig = {};
+export type FileConfig = {
+  awsRegion: string;
+  awsS3BucketName: string;
+  awsAccessKey: string;
+  awsSecretKey: string;
+};
 
 export default registerAs<FileConfig>('file', () => {
-  return {};
+  return {
+    awsRegion: process.env.AWS_REGION,
+    awsS3BucketName: process.env.AWS_S3_BUCKET_NAME,
+    awsAccessKey: process.env.AWS_ACCESS_ACCESS_KEY,
+    awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
+  };
 });
