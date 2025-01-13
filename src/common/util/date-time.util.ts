4016c0ef0a608bf483772e053159f59a50135f3f
Lee Gyumin <legm0310@gmail.com>
2025-01-13 23:28:34 +0900

    ⚡️ Feat: 월별 Date값 변환 유틸 메소드 추가

--- src/common/util/date-time.util.ts
+++ src/common/util/date-time.util.ts
@@ -4,4 +4,24 @@ export class DateTimeUtil {
     const kst = new Date(date.getTime() + kstOffset);
     return kst.toISOString().replace('Z', '+09:00');
   }
+
+  static getMonthStartAndEnd(date: Date): { start: Date; end: Date } {
+    const start = Date.UTC(date.getFullYear(), date.getMonth(), 1);
+    const end = Date.UTC(
+      date.getFullYear(),
+      date.getMonth() + 1,
+      0,
+      23,
+      59,
+      59,
+      999,
+    );
+    return { start: new Date(start), end: new Date(end) };
+  }
+
+  static getDayStartAndEnd(date: Date): { start: Date; end: Date } {
+    const start = Date.UTC(date.getFullYear(), date.getMonth(), 1);
+    const end = Date.UTC(date.getFullYear(), date.getMonth(), 1);
+    return { start: new Date(start), end: new Date(end) };
+  }
 }
