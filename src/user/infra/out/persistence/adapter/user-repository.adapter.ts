84713397a17aa19c6c04d75d3f53323caf132a1e
Lee Gyumin <legm0310@gmail.com>
2025-05-09 03:51:12 +0900

    ✨ Feat: 팔로워 팔로잉 조회

--- src/user/infra/out/persistence/adapter/user-repository.adapter.ts
+++ src/user/infra/out/persistence/adapter/user-repository.adapter.ts
@@ -9,9 +9,7 @@ import { HandleUserPort } from 'src/user/domain/port/out/handle-user.port';
 import { LoadUserPort } from 'src/user/domain/port/out/load-user.port';
 import { FollowEntity } from '../entity/follow.entity';
 import {
-  Follower,
-  Following,
-  SearchedUser,
+  FollowUser, SearchedUser, UserSummary,
 } from '../../../../domain/model/user-read-model';
 
 export class UserRepositoryAdapter implements HandleUserPort, LoadUserPort {
@@ -84,44 +82,103 @@ export class UserRepositoryAdapter implements HandleUserPort, LoadUserPort {
     return user ? UserMapper.toDomain(user) : null;
   }
 
+  /**
+   * @TODO 테이블 정규화(맞팔여부)
+   *   팔로우 테이블 isMutualFollow 필드 추가
+   *   이벤트 기반 동기화
+   *
+   * innerjoin -> 팔로워들 조회
+   * leftjoin -> 팔로워들 팔로잉 여부 조회
+   * */
   async findFollowers(
     userId: number,
     onlyApproved: boolean,
-  ): Promise<Follower[]> {
-    const where = { followingId: userId };
-    if (onlyApproved) {
-      where['isApproved'] = true;
-    }
-    const followEntities = await this._followRepository.find({
-      where: where,
-      relations: {
-        follower: true,
-      },
-    });
-    return followEntities.map((e) => ({
-      isApproved: e.isApproved,
-      follower: UserMapper.toReadModel(e.follower),
-    }));
+  ): Promise<FollowUser[]> {
+    const followers = await this._followRepository
+      .createQueryBuilder('f')
+      .innerJoin('f.follower', 'user') // 팔로워 유저 정보
+      .leftJoin(
+        FollowEntity,
+        'reverse',
+        'reverse.followerId = :myId AND reverse.followingId = user.id',
+        { myId: userId })
+      .where('f.followingId = :myId', { myId: userId })
+      .andWhere(onlyApproved ? 'f.isApproved = true' : '1=1')
+      .select([
+        'user.id AS id',
+        'user.email AS email',
+        'user.nickname AS nickname',
+        'user.image AS image',
+        'f.isApproved AS isApproved',
+        `CASE 
+          WHEN reverse.followerId IS NOT NULL 
+            AND reverse.isApproved = true 
+            AND f.isApproved = true 
+          THEN true 
+          ELSE false 
+         END AS isMutualFollow`,
+      ])
+      .getRawMany();
+    console.log(followers)
+
+    return followers.map((follower) => {
+      const { isApproved, isMutualFollow, ...rest} = follower
+      return {
+        user: rest as UserSummary,
+        isApproved : Boolean(isApproved),
+        isMutualFollow: Boolean(parseInt(isMutualFollow)),
+      }
+    })
   }
 
+  /**
+   * @TODO 테이블 정규화(맞팔여부)
+   *   팔로우 테이블 isMutualFollow 필드 추가
+   *   이벤트 기반 동기화
+   *
+   * 팔로잉하는 사람들 조회
+   * inner join -> 팔로잉 조회
+   * left join -> 팔로잉들의 팔로워 여부 조회
+   */
   async findFollowing(
     userId: number,
     onlyApproved: boolean,
-  ): Promise<Following[]> {
-    const where = { followerId: userId };
-    if (onlyApproved) {
-      where['isApproved'] = true;
-    }
-    const followEntities = await this._followRepository.find({
-      where: where,
-      relations: {
-        following: true,
-      },
-    });
-    return followEntities.map((e) => ({
-      isApproved: e.isApproved,
-      following: UserMapper.toReadModel(e.following),
-    }));
+  ): Promise<FollowUser[]> {
+
+    const followings = await this._followRepository
+      .createQueryBuilder('f')
+      .innerJoin('f.following', 'user')
+      .leftJoin(
+        FollowEntity,
+        'reverse',
+        'reverse.followerId = user.id AND reverse.followingId = :myId',
+        { myId: userId })
+      .where('f.followerId = :myId', { myId : userId })
+      .andWhere(onlyApproved ? 'f.isApproved = true' : '1=1')
+      .select([
+        'user.id AS id',
+        'user.email AS email',
+        'user.nickname AS nickname',
+        'user.image AS image',
+        'f.isApproved AS isApproved',
+        `CASE 
+          WHEN reverse.followerId IS NOT NULL 
+            AND reverse.isApproved = true 
+            AND f.isApproved = true 
+          THEN true 
+          ELSE false 
+         END AS isMutualFollow`,
+      ])
+      .getRawMany();
+
+    return followings.map((following) => {
+      const { isApproved, isMutualFollow, ...rest} = following
+      return {
+        user: rest as UserSummary,
+        isApproved : Boolean(isApproved),
+        isMutualFollow: Boolean(parseInt(isMutualFollow)),
+      }
+    })
   }
 
   async searchUsersByNickname(
