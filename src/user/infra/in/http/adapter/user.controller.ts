38d62e2cb53844863e428b5631b2fa89199e2ca6
Lee Gyumin <legm0310@gmail.com>
2025-05-12 18:01:46 +0900

    🐛 Fix: 컨트롤러 문법 오류 수정

--- src/user/infra/in/http/adapter/user.controller.ts
+++ src/user/infra/in/http/adapter/user.controller.ts
@@ -26,7 +26,11 @@ import {
   RejectFollowCommand,
   UnfollowCommand,
 } from '../../../../application/command/user.command';
-import { GetFollowerQuery, GetFollowingQuery, SearchUsersQuery } from '../../../../application/query/user.query';
+import {
+  GetFollowerQuery,
+  GetFollowingQuery,
+  SearchUsersQuery,
+} from '../../../../application/query/user.query';
 import { CreateUserDto, UpdateUserDto } from '../dto/user.req';
 import { ShowUserResDto } from '../dto/user.res';
 
@@ -125,24 +129,24 @@ export class UserController {
 
   @Get('follower')
   @UseGuards(JwtAccessAuthGuard)
-  async getFollowers(@UserId() userId: number,) {
-    return await this._queryBus.execute(new GetFollowerQuery(userId, false))
+  async getFollowers(@UserId() userId: number) {
+    return await this._queryBus.execute(new GetFollowerQuery(userId, false));
   }
 
   @Get('follower/:id')
   async testGetFollowers(@Param('id') userId: number) {
-    return await this._queryBus.execute(new GetFollowerQuery(userId, false))
+    return await this._queryBus.execute(new GetFollowerQuery(userId, false));
   }
 
   @Get('following')
   @UseGuards(JwtAccessAuthGuard)
-  async getFollowings(@UserId() userId: number){
-    return await this._queryBus.execute(new GetFollowingQuery(userId, false))
+  async getFollowings(@UserId() userId: number) {
+    return await this._queryBus.execute(new GetFollowingQuery(userId, false));
   }
 
   @Get('following/:id')
   async testGetFollowings(@Param('id') userId: number) {
-    return await this._queryBus.execute(new GetFollowingQuery(userId, false))
+    return await this._queryBus.execute(new GetFollowingQuery(userId, false));
   }
 
   @Patch('follow/:id/approve')
