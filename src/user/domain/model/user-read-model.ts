802d3a70d8bc20559d6fdfda7ae169d4368e002c
Lee Gyumin <legm0310@gmail.com>
2025-06-20 23:35:58 +0900

    ✨ Feat: 팔로우 페이지네이션 리드모델

--- src/user/domain/model/user-read-model.ts
+++ src/user/domain/model/user-read-model.ts
@@ -62,4 +62,4 @@ export class FollowUser {
   isMutualFollow: boolean;
 }
 
-export type PagedFollowUser = PageResult<FollowUser, number>;
+export type PagedFollowUser = PageResult<FollowUser, string>;
