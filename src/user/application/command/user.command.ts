2cc75dbfc58afde4dee7a1226b06bd20fc618a78
Lee Gyumin <legm0310@gmail.com>
2025-07-24 23:59:06 +0900

    ✨ Feat: 팔로우 요청 승인 전 요청 취소

--- src/user/application/command/user.command.ts
+++ src/user/application/command/user.command.ts
@@ -32,6 +32,13 @@ export class UnfollowCommand {
   ) {}
 }
 
+export class CancelFollowRequestCommand {
+  constructor(
+    public readonly followerId: number,
+    public readonly followingId: number,
+  ) {}
+}
+
 export class ApproveFollowCommand {
   constructor(
     public readonly followerId: number,
