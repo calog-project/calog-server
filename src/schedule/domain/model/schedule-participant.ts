0ad170beb69b1828b995f465ef2fad722a4d1830
Lee Gyumin <legm0310@gmail.com>
2025-08-08 23:58:37 +0900

    ✨ Feat: 일정 참여자 상태를 위한 도메인 모델

---
+++ src/schedule/domain/model/schedule-participant.ts
@@ -0,0 +1,17 @@
+interface ScheduleParticipantProps {
+  scheduleId: number;
+  userId: number;
+  status: unknown;
+  categoryId: number;
+}
+
+export interface ScheduleParticipantPrimitives {
+  scheduleId: number;
+  userId: number;
+  status: unknown;
+  categoryId: number;
+}
+
+export class ScheduleParticipant {
+  constructor() {}
+}
