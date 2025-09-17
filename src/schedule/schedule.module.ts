bb730fbb6fc0854f45072651b2a2b08e3fcaa873
Lee Gyumin <legm0310@gmail.com>
2025-09-17 23:24:56 +0900

    ✨ Feat: 일정 모듈에 uow 어댑터 추가

--- src/schedule/schedule.module.ts
+++ src/schedule/schedule.module.ts
@@ -7,6 +7,8 @@ import { CategoryController } from './infra/in/http/adapter/category.controller'
 import { CalendarService } from './application/service/calendar.service';
 import { ScheduleService } from './application/service/schedule.service';
 import { CategoryService } from './application/service/category.service';
+
+import { TypeormUowAdapter } from '../common/adapter/typeorm-uow.adapter';
 import { ScheduleRepositoryAdapter } from './infra/out/persistence/adapter/schedule-repository.adapter';
 import { ScheduleParticipantRepositoryAdapter } from './infra/out/persistence/adapter/schedule-participant-repository.adapter';
 import { CategoryRepositoryAdapter } from './infra/out/persistence/adapter/category-repository.adapter';
@@ -21,6 +23,7 @@ import { UpdateCategoryUseCaseSymbol } from './domain/port/in/update-category.us
 import { GetCategoryUseCaseSymbol } from './domain/port/in/get-category.usecase';
 import { DeleteCategoryUseCaseSymbol } from './domain/port/in/delete-category.usecase';
 
+import { UnitOfWorkPortSymbol } from '../common/port/uow.port';
 import { HandleSchedulePortSymbol } from './domain/port/out/handle-schedule.port';
 import { LoadSchedulePortSymbol } from './domain/port/out/load-schedule.port';
 import { HandleScheduleParticipantPortSymbol } from './domain/port/out/handle-schedule-participant.port';
@@ -52,6 +55,13 @@ import {
   GetCategoriesByUserIdHandler,
 } from './application/query/category.query-handler';
 import { UserModule } from 'src/user/user.module';
+
+const adapterProvider = [
+  {
+    provide: UnitOfWorkPortSymbol,
+    useClass: TypeormUowAdapter,
+  },
+];
 
 const handlerProvider = [
   CreateScheduleHandler,
@@ -128,7 +138,12 @@ const useCaseProvider = [
 @Module({
   imports: [SchedulePersistenceModule, UserModule],
   controllers: [CalendarController, ScheduleController, CategoryController],
-  providers: [...repositoryProvider, ...useCaseProvider, ...handlerProvider],
+  providers: [
+    ...adapterProvider,
+    ...repositoryProvider,
+    ...useCaseProvider,
+    ...handlerProvider,
+  ],
   exports: [
     CreateScheduleUseCaseSymbol,
     GetScheduleUseCaseSymbol,
